7516561edd77b4e3fed7f97519d7eb50a212b7d3
Bruno Jose Scatena <bjscatena@macae.unimed.com.br>
2020-01-03 15:51:11 -0300

    course progress

---
+++ src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+interface BreadCrumbItem {
+  text: string;
+  link?: string;
+}
+
+@Component({
+  selector: 'app-bread-crumb',
+  templateUrl: './bread-crumb.component.html',
+  styleUrls: ['./bread-crumb.component.css']
+})
+export class BreadCrumbComponent {
+  @Input() items: Array<BreadCrumbItem> = [];
+
+  constructor() {}
+
+  isTheLastItem(item: BreadCrumbItem) {
+    const index = this.items.indexOf(item);
+    return index + 1 === this.items.length;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [CommonModule, ReactiveFormsModule]
+  declarations: [BreadCrumbComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  exports: [CommonModule, ReactiveFormsModule, BreadCrumbComponent]
 })
 export class SharedModule {}
